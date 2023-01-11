e804c8788611941479ae48931d33f34e5bcebd9c
Kevin Abel <kevin.abel.0@gmail.com>
2023-01-11 13:13:35 -0600

    Support jdk names/versions from the 0.11 index (#7)
    Got things to map over pretty consistently. There are a few exceptions/ambiguities:
    
    * openjdk-ri (not in disco)
    * ibm (not in disco)
    * some zulu legacy versions don't consistently map the x.0.0 versions
    
    Also bumps npm packages for audit.
    
    Also uses semver package to sort the jdks in descending version order (rather than in whatever order they come from the disco API).

--- src/index.js
+++ src/index.js
@@ -1,98 +1,173 @@
+/* eslint-disable no-console */
+
 import axios from 'axios';
 import { writeFile } from 'node:fs';
+import semver from 'semver';
 
 const windowsOpts = {
   operatingSystem: 'windows',
-  archiveType:'zip',
-  architectures: ['amd64', 'x86']
-}
+  archiveTypes: ['exe', 'tar.gz', 'zip'],
+  architectures: ['amd64', 'x86'],
+};
 
 const macOpts = {
   operatingSystem: 'macos',
-  archiveType:'tar.gz',
-  architectures: ['amd64', 'arm64']
-}
+  archiveTypes: ['dmg', 'zip', 'tar.gz'],
+  architectures: ['amd64', 'arm64'],
+};
 
 const linuxOpts = {
   operatingSystem: 'linux',
-  archiveType:'tar.gz',
-  architectures: ['amd64', 'x86', 'arm', 'arm64', 'aarch64']
-}
-
-const index = {
-  windows: {
-
-  },
-  linux: {
-
-  },
-  darwin: {
-
-  }
+  archiveTypes: ['zip', 'tar.gz'],
+  architectures: ['amd64', 'x86', 'arm', 'arm64'],
 };
 
-
+const index = {
+  windows: {},
+  linux: {},
+  darwin: {},
+};
 
 async function fetchPackages(pkgOptions) {
-  let architecture = ''
-  pkgOptions.architectures.forEach(arch => {
-    architecture.concat(`&architecture=${arch}`);
+  let architecture = '';
+  pkgOptions.architectures.forEach((arch) => {
+    architecture += `&architecture=${arch}`;
   });
-  const url = `https://api.foojay.io/disco/v3.0/packages/jdks?operating_system=${pkgOptions.operatingSystem}${architecture}&archive_type=${pkgOptions.archiveType}`;
-  return await axios({
+  let archiveType = '';
+  pkgOptions.archiveTypes.forEach((type) => {
+    archiveType += `&archive_type=${type}`;
+  });
+  const url = `https://api.foojay.io/disco/v3.0/packages/jdks?operating_system=${pkgOptions.operatingSystem}${architecture}&javafx_bundled=false${archiveType}`;
+  return axios({
     url,
-    method: 'get'
-  }).then((response) => response.data.result)
+    method: 'get',
+  }).then((response) => response.data.result);
 }
 
 function mapOS(os) {
-  if(os === 'macos') {
+  if (os === 'macos') {
     return 'darwin';
   }
   return os;
 }
 
 function mapArchitecture(os, architecture) {
-  if(architecture === 'x64') {
+  if (architecture === 'x64') {
     return 'amd64';
   }
-  if(architecture === 'x86') {
-    return '386'
+  if (architecture === 'x86') {
+    return '386';
   }
-  if(os === 'macos' && architecture === 'aarch64') {
-    return 'arm64'
+  if (architecture === 'aarch64') {
+    return 'arm64';
   }
   return architecture;
 }
 
 function mapArchiveType(archiveType) {
-  if(archiveType === 'tar.gz') {
-    return 'tgz+';
-  } else if (archiveType === 'zip') {
-    return 'zip+';
+  switch (archiveType) {
+    case 'tar.gz':
+      return 'tgz+';
+    case 'zip':
+      return 'zip+';
+    case 'dmg':
+      return 'dmg+';
+    case 'exe':
+      return 'exe+';
+    default:
+      return '';
   }
 }
 
-function mapVersion(java_version) {
-  let version = java_version.split('+')[0];
+function mapVersion(javaVersion) {
+  const [version] = javaVersion.split('+');
   const semvers = version.split('.');
-  version = `${semvers[0]}`;
-  if(semvers[1]){
-    version = `${version}.${semvers[1]}`
+  let versionOut = `${semvers[0]}`;
+  if (semvers[1]) {
+    versionOut += `.${semvers[1]}`;
   }
-  if(semvers[2]){
-    version = `${version}.${semvers[2]}`
+  if (semvers[2]) {
+    versionOut += `.${semvers[2]}`;
   }
-  if(semvers[3]){
-    version = `${version}-${semvers[3]}`
+  if (semvers[3]) {
+    versionOut += `-${semvers[3]}`;
   }
-  return version;
+  return versionOut;
 }
 
-function parsePackages(packageList, archiveType) {
-  archiveType = mapArchiveType(archiveType);
-  packageList.forEach((jdkPkg) => {
+function mapLegacyVersion(jdk) {
+  const [version, plusVersion] = jdk.java_version.split('+');
+  const semvers = version.split('.');
+  if (!/^graalvm_/.test(jdk.distribution)) {
+    semvers.unshift('1');
+    while (semvers.length < 3) {
+      semvers.push('0');
+    }
+    if (version.indexOf('.') === -1 && !/^oracle/.test(jdk.distribution) &&
+      (plusVersion !== undefined || jdk.distribution !== 'zulu')) {
+      semvers.push('0');
+    }
+    if (jdk.distribution === 'corretto') {
+      const correttoRest = jdk.distribution_version.split('.').slice(3).join('.');
+      if (correttoRest.length) {
+        semvers[semvers.length - 1] += `.${correttoRest}`;
+      }
+    } else if (jdk.distribution === 'liberica' || jdk.distribution === 'zulu') {
+      if (semvers[1] === '8') {
+        semvers.splice(2, 1);
 
+        if (jdk.distribution !== 'zulu') {
+          semvers.push(plusVersion);
+        }
+      }
+    }
+  } else {
+    while (semvers.length < 3) {
+      semvers.push('0');
+    }
+  }
+  let versionOut = `${semvers[0]}`;
+  if (semvers[1]) {
+    versionOut += `.${semvers[1]}`;
+  }
+  if (semvers[2]) {
+    versionOut += `.${semvers[2]}`;
+  }
+  if (semvers[3]) {
+    versionOut += `-${semvers[3]}`;
+  }
+  return versionOut;
+}
+
+function mapLegacyDistribution(distribution) {
+  switch (distribution) {
+    case 'oracle_open_jdk':
+      return 'openjdk';
+    case 'graalvm_ce8':
+      return 'graalvm-ce-java8';
+    case 'graalvm_ce19':
+      return 'graalvm-ce-java19';
+    case 'graalvm_ce17':
+      return 'graalvm-ce-java17';
+    case 'graalvm_ce16':
+      return 'graalvm-ce-java16';
+    case 'graalvm_ce11':
+      return 'graalvm-ce-java11';
+    case 'corretto':
+      return 'amazon-corretto';
+    case 'aoj_openj9':
+      return 'adopt-openj9';
+    case 'aoj':
+      return 'adopt';
+    case 'oracle':
+      return '';
+    default:
+      return null;
+  }
+}
+
+function parsePackages(packageList) {
+  packageList.forEach((jdkPkg) => {
     const operatingSystem = mapOS(jdkPkg.operating_system);
     const architecture = mapArchitecture(jdkPkg.operating_system, jdkPkg.architecture);
     const link = jdkPkg.links.pkg_download_redirect;
@@ -101,7 +176,8 @@ function parsePackages(packageList, archiveType) {
     }
 
     const version = mapVersion(jdkPkg.java_version);
-  
+    const type = mapArchiveType(jdkPkg.archive_type);
+
     if (!index[operatingSystem][architecture]) {
       index[operatingSystem][architecture] = {};
     }
@@ -110,16 +186,72 @@ function parsePackages(packageList, archiveType) {
       index[operatingSystem][architecture][`jdk@${jdkPkg.distribution}`] = {};
     }
     const jdkDist = index[operatingSystem][architecture][`jdk@${jdkPkg.distribution}`];
-    jdkDist[version] = `${archiveType}${link}`;
+    const jdkLink = `${type}${link}`;
+    jdkDist[version] = jdkLink;
+
+    const legacyVersion = mapLegacyVersion(jdkPkg);
+    if (legacyVersion !== version) {
+      jdkDist[legacyVersion] = jdkLink;
+    }
+
+    let legacyJdk = mapLegacyDistribution(jdkPkg.distribution);
+    if (legacyJdk !== null) {
+      if (legacyJdk !== '') {
+        legacyJdk = `@${legacyJdk}`;
+      }
+
+      if (!index[operatingSystem][architecture][`jdk${legacyJdk}`]) {
+        index[operatingSystem][architecture][`jdk${legacyJdk}`] = jdkDist;
+      }
+    }
+
+    if (/^graalvm/.test(jdkPkg.distribution)) {
+      if (!index[operatingSystem][architecture]['jdk@graalvm']) {
+        index[operatingSystem][architecture]['jdk@graalvm'] = {};
+      }
+
+      index[operatingSystem][architecture]['jdk@graalvm'][legacyVersion] = jdkLink;
+    }
   });
 }
 
-parsePackages(await fetchPackages(windowsOpts), windowsOpts.archiveType);
-parsePackages(await fetchPackages(macOpts), macOpts.archiveType);
-parsePackages(await fetchPackages(linuxOpts), linuxOpts.archiveType);
+parsePackages(await fetchPackages(windowsOpts));
+parsePackages(await fetchPackages(macOpts));
+parsePackages(await fetchPackages(linuxOpts));
 
+// sort the index by version for each os, arch, jdk
+Object.keys(index).forEach((os) => {
+  Object.keys(index[os]).forEach((architecture) => {
+    Object.keys(index[os][architecture]).forEach((jdk) => {
+      const orderedJdk = Object.keys(index[os][architecture][jdk])
+        .sort(
+          (a, b) => {
+            let [verA, verB] = [a, b];
+            for (let i = (a.match(/\./g) || []).length; i < 2; ++i) {
+              verA += '.0';
+            }
+            for (let i = (b.match(/\./g) || []).length; i < 2; ++i) {
+              verB += '.0';
+            }
 
-writeFile('index.json', JSON.stringify(index, null, 4), (err) => {
+            return semver.compare(verA, verB);
+          },
+        )
+        .reverse()
+        .reduce(
+          (obj, version) => {
+            const myObj = obj;
+            myObj[version] = index[os][architecture][jdk][version];
+            return myObj;
+          },
+          {},
+        );
+      index[os][architecture][jdk] = orderedJdk;
+    });
+  });
+});
+
+writeFile('index.json', `${JSON.stringify(index, null, 4)}\n`, (err) => {
   if (err) throw err;
   console.log('The file has been saved!');
 });
